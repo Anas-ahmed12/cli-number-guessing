c0e7f2480eaa0e309d705bd54b01634e3f1407e0
Anas-ahmed12 <164876298+Anas-ahmed12@users.noreply.github.com>
2024-04-23 20:15:45 -0700

    created number guessing game

---
+++ index.ts
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const ramdomNumber = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt([
+  {
+    name: "userGuessedNumber",
+    type: "number",
+    message: "Please guess a number between 1-6",
+  },
+]);
+
+if (answer.userGuessedNumber === ramdomNumber) {
+  console.log("Congtratulation! You Guess right number.");
+} else {
+  console.log("You guess wrong number!");
+}
